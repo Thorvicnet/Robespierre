27c77ee6318c6adc6976983783806dfbf84c311f
Thorvicnet <thorvicnet@gmail.com>
2024-12-21 23:40:11 +0100

    you can play in main

--- src/main.c
+++ src/main.c
@@ -3,17 +3,21 @@
 #include "types.h"
 #include <locale.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, ""); // Enable Unicode Handling
 
   Board *board = board_init();
-  board_empty(board);
-  board_set(board, 3 + 3 * 8, WHITE_BISHOP);
-  board_set(board, 2 + 2 * 8, BLACK_PAWN);
-  board_info(board);
-  wprintf(L"%d", move_check_validity(board, (int[]){3, 3}, (int[]){0, 0}));
+  int orig[2], dest[2];
+  while (true) {
+    board_info(board);
+    scanf("%d %d, %d %d", &orig[0], &orig[1], &dest[0], &dest[1]);
+    move(board, (Move){board_get(board, orig[0] + orig[1] * 8),
+                       {orig[0], orig[1]},
+                       {dest[0], dest[1]}});
+  }
 
   return EXIT_SUCCESS;
 }
