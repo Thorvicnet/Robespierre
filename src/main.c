9a50d3600822e69c56fd1569a6f3d7c0ec67793b
Thorvicnet <thorvicnet@gmail.com>
2025-01-11 07:43:06 +0100

    fixed UAF in main.c

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <wchar.h>
- 
+
 void test_print_moves(MoveList l) {
   for (int i = 0; i < l.count; i++) {
     char *mv = move_to_algebric(l.moves[i]);
@@ -19,141 +19,125 @@ void test_print_moves(MoveList l) {
   }
   wprintf(L"\n");
 }
- 
-int command(char* strmove, bool *player1, bool *player2, Board *board){
-    if (strncmp(strmove, "init", 4) == 0){  
-        if (strcmp(strmove , "initpp") == 0){
-          *player1 = true;
-          *player2 = true;
-        }
-        else if (strcmp(strmove, "initbp") == 0){
-          *player1 = false;
-          *player2 = true;
-  
-        }
-  
-        else if (strcmp(strmove,"initpb") == 0){
-          *player1 = true;
-          *player2 = false;
-  
-        }
-  
-        else if (strcmp(strmove, "initbb") == 0){
-          *player1 = false;
-          *player2 = false;
-        }
-        free(board->history->list_of_move);
-        free(board->history);
-        board_free(board);
-        board = board_init();
-        threat_board_update(board);
-        return 2;
-    }
-    else if (strncmp(strmove, "pbb", 3) == 0){
-        board_bb_info(board);
-        return -1;
-    }
-    else if (strncmp(strmove, "ppm", 3) == 0){
 
-      test_print_moves(move_possible(board));
-      return -1;
-    }
-    else if (strncmp(strmove, "exit", 4) == 0){
+bool player1 = true;
+bool player2 = true;
 
-      free(board->history->list_of_move);
-      free(board->history);
-      board_free(board);
-      exit(EXIT_SUCCESS);
+int command(char *strmove, Board **board) {
+  if (!strncmp(strmove, "init", 4)) {
+    if (!strcmp(strmove, "initpp")) {
+      player1 = true;
+      player2 = true;
+    } else if (!strcmp(strmove, "initbp")) {
+      player1 = false;
+      player2 = true;
+    } else if (!strcmp(strmove, "initpb")) {
+      player1 = true;
+      player2 = false;
+    } else if (!strcmp(strmove, "initbb")) {
+      player1 = false;
+      player2 = false;
     }
-    else{
-      return move(board, algebric_to_move(strmove, board));
+    free((*board)->history->list_of_move);
+    free((*board)->history);
+    board_free(*board);
+    *board = board_init();
+    threat_board_update(*board);
+    return 2;
+  } else if (!strncmp(strmove, "pbb", 3)) {
+    board_bb_info(*board);
+    return -1;
+  } else if (!strncmp(strmove, "ppm", 3)) {
+    test_print_moves(move_possible(*board));
+    return -1;
+  } else if (!strncmp(strmove, "exit", 4)) {
+    free((*board)->history->list_of_move);
+    free((*board)->history);
+    board_free(*board);
+    exit(EXIT_SUCCESS);
+  } else {
+    return move(*board, algebric_to_move(strmove, *board));
   }
 }
 
-int bot_turn(Board *board){
+int bot_turn(Board *board) {
   board_info(board);
   Move bot = choose(board);
- 
+
   char *mv = move_to_algebric(bot);
   wprintf(L"%s\n", mv);
   free(mv);
- 
+
   return move(board, bot);
-  
 }
 
 int main(void) {
   setlocale(LC_ALL, ""); // Enable Unicode Handling
   bb_magic_init();
- 
+
   Board *board = board_init();
   char strmove[15];
   int res;
-  bool player1 = true;
-  bool player2 = true;
 
- 
   while (true) {
     board_info(board);
-    if (player1){
+    if (player1) {
       wprintf(L"\nPlayer Turn white\n");
       res = -1;
       while (res) {
         scanf("%s", strmove);
-        res = command(strmove, &player1, &player2, board);
-        if (res == 2) break;
+        res = command(strmove, &board);
+        if (res == 2)
+          break;
       }
-      if (res == 2){
+      if (res == 2) {
         board_info(board);
         continue;
       }
-    }
-    else {
-      wprintf(L"\nBOT WHITE\n");  
+    } else {
+      wprintf(L"\nBOT WHITE\n");
       res = bot_turn(board);
-      if (res){
+      if (res) {
         wprintf(L"Bot fail, bot dumb\n");
         while (res) {
           scanf("%s", strmove);
-          res = command(strmove, &player1, &player2, board);
+          res = command(strmove, &board);
         }
-        continue;    
+        continue;
       }
     }
-    
+
     board_info(board);
-    if (player2){
+    if (player2) {
       wprintf(L"\nPlayer Turn black\n");
       res = -1;
       while (res) {
         scanf("%s", strmove);
-        res = command(strmove, &player1, &player2, board);
-        if (res == 2) break;
+        res = command(strmove, &board);
+        if (res == 2)
+          break;
       }
-      if (res == 2){
+      if (res == 2) {
 
         continue;
       }
-    }
-    else {
-      wprintf(L"\nBOT black\n");  
+    } else {
+      wprintf(L"\nBOT BLACK\n");
       res = bot_turn(board);
-      if (res){
+      if (res) {
         wprintf(L"Bot fail, bot dumb\n");
         while (res) {
           scanf("%s", strmove);
-          res = command(strmove, &player1, &player2, board);
+          res = command(strmove, &board);
         }
-        continue;    
+        continue;
       }
     }
-    
-    
   }
- 
+
   free(board->history->list_of_move);
   free(board->history);
   board_free(board);
- 
+
   return EXIT_SUCCESS;
 }
