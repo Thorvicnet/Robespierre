d4659b03fbcb8959183c746ab71c022bfaff5ec5
Thorvicnet <thorvicnet@gmail.com>
2025-01-15 23:19:50 +0100

    threading perft 3,5s on my thinkpad

--- src/perft.c
+++ src/perft.c
@@ -4,11 +4,79 @@
 #include "threat.h"
 #include "types.h"
 #include <assert.h>
+#include <pthread.h>
 #include <stdbool.h>
+#include <sys/time.h>
 #include <time.h>
 #include <wchar.h>
+#define DEPTH 7
+#define NUM_THREADS 8
 
-const int DEPTH = 7;
+typedef struct {
+  Board *board;
+  Move move; // Single move instead of array
+  int depth;
+  int *result;
+  pthread_mutex_t *mutex;
+} ThreadArgs;
+
+int count_moves(Board *board, int depth);
+
+void *thread_count_moves(void *arg) {
+  ThreadArgs *args = (ThreadArgs *)arg;
+  Board *board_cpy = board_copy(args->board);
+  Undo undo;
+  int local_count = 0;
+
+  int res = move_make(board_cpy, &(args->move), &undo);
+  if (!res) {
+    local_count = count_moves(board_cpy, args->depth - 1);
+    move_undo(board_cpy, &(args->move), &undo);
+  }
+
+  pthread_mutex_lock(args->mutex);
+  *args->result += local_count;
+  pthread_mutex_unlock(args->mutex);
+
+  board_free(board_cpy);
+  return NULL;
+}
+
+int parallel_count_moves(Board *board, int depth) {
+  if (depth <= 1) {
+    return count_moves(board, depth);
+  }
+
+  Move moves[MAX_MOVES];
+  int moves_count = move_possible(board, moves);
+  int count = 0;
+  pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+  // Create threads based on actual move count
+  int num_threads = moves_count < NUM_THREADS ? moves_count : NUM_THREADS;
+  pthread_t *threads = malloc(moves_count * sizeof(pthread_t));
+  ThreadArgs *args = malloc(moves_count * sizeof(ThreadArgs));
+
+  // Launch one thread per move
+  for (int i = 0; i < moves_count; i++) {
+    args[i].board = board;
+    args[i].move = moves[i];
+    args[i].depth = depth;
+    args[i].result = &count;
+    args[i].mutex = &mutex;
+    pthread_create(&threads[i], NULL, thread_count_moves, &args[i]);
+  }
+
+  // Join all threads
+  for (int i = 0; i < moves_count; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  pthread_mutex_destroy(&mutex);
+  free(threads);
+  free(args);
+  return count;
+}
 
 int count_moves(Board *board, int depth) {
   if (depth == 0) {
@@ -35,14 +103,18 @@ int main() {
   threat_board_update(board);
 
   int count[DEPTH];
+  struct timeval start, end;
 
   for (int depth = 0; depth < DEPTH; depth++) {
-    clock_t start = clock(), diff;
-    count[depth] = count_moves(board, depth);
-    diff = clock() - start;
-    int msec = diff * 1000 / CLOCKS_PER_SEC;
-    wprintf(L"- depth %d: %d, took %ds %dms, %d clocks\n", depth, count[depth],
-            msec / 1000, msec % 1000, diff);
+    gettimeofday(&start, NULL);
+    count[depth] = parallel_count_moves(board, depth);
+    gettimeofday(&end, NULL);
+
+    long seconds = end.tv_sec - start.tv_sec;
+    long microseconds = end.tv_usec - start.tv_usec;
+    double elapsed = seconds + microseconds * 1e-6;
+
+    wprintf(L"- depth %d: %d, took %.3fs\n", depth, count[depth], elapsed);
   }
 
   assert(count[0] == 1);
